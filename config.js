642da9720dc81909ffa7d25cc5b52385bd53c686
qweweee84 <qweweee84@gmail.com>
2025-06-26 10:14:22 +0900

    250626_1014

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 const BASE_URL = "https://qweweee84.github.io/deepdive/";
 
 const infographicPages = [
-    { title: "공황장애 인포그래픽", filename: "panic-disorder.html" },  
+    { title: "근감소증 예방 및 관리 인포그래픽", filename: "근감소증 예방 및 관리.html" },
+    { title: "퇴행성 무릎 관절염 관리 인포그래픽", filename: "퇴행성 무릎 관절염 관리.html" },
+    { title: "대상포진 예방 및 관리 인포그래픽", filename: "대상포진 예방 및 관리.html" },
+    { title: "현명한 상속 및 증여 전략 인포그래픽", filename: "현명한 상속 및 증여 전략.html" },
+    { title: "뇌 건강 및 치매 예방 인포그래픽", filename: "뇌 건강 및 치매 예방.html" },
+    { title: "공황장애 인포그래픽", filename: "공황장애 극복.html" },
 ];
 
 window.addEventListener("DOMContentLoaded", () => {
